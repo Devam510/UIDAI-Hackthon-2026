816d0583d5530266d9c87825305300487f8880e4
Devam Patel <pateldevam5354@gmail.com>
2026-01-18 20:17:55 +0530

    Fix CORS: Whitelist deployment frontend URL

--- uidai_hackathon/backend/main.py
+++ uidai_hackathon/backend/main.py
@@ -42,7 +42,12 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "*"],
+    allow_origins=[
+        "http://localhost:5173", 
+        "http://localhost:3000", 
+        "https://uidai-hackathon-2026-ui.onrender.com",
+        "*"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
